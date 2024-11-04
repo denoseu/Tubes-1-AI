5c8d351f805faaf29a26462e01bdad9d8d4aae92
angiekierra <angie.kierra@gmail.com>
2024-11-04 21:27:53 +0700

    fix: kwargs

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ genetic = GeneticAlgorithm()
 while True:
     magic_cube = MagicCube(n)
     initial_cube = magic_cube.cube.copy()
-    # Continuously prompt for algorithm selection until a valid option is chosen
+
     while True:
         print("\nPilih Algoritma:")
         print("1. Steepest Ascent Hill Climbing")
@@ -32,7 +32,7 @@ while True:
         print("5. Simulated Annealing")
         print("6. Genetic Algorithm")
         print("7. Exit")
-        
+
         try:
             choice = int(input("Masukkan pilihan (1-7): "))
             if 1 <= choice <= 7:
@@ -41,8 +41,9 @@ while True:
                 print("Input tidak valid. Masukkan nomor dari 1 sampai 7.")
         except ValueError:
             print("Input tidak valid. Masukkan nomor dari 1 sampai 7.")
-    
-    # Handle user choice
+
+    # Prepare algorithm and kwargs based on choice
+    kwargs = {}  # Default kwargs as empty
     if choice == 1:
         algorithm_manager.setStrategy(steepestAscent)
     elif choice == 2:
@@ -55,13 +56,16 @@ while True:
         algorithm_manager.setStrategy(simulatedAnnealing)
     elif choice == 6:
         algorithm_manager.setStrategy(genetic)
+        population_size = int(input("Masukkan jumlah populasi awal: "))
+        generations = int(input("Masukkan banyak iterasi: "))
+        kwargs = {"population_size": population_size, "n": n, "generations": generations}
     elif choice == 7:
         print("Exiting program.")
         break
 
     # Run the selected algorithm
     start_time = time.time()
-    final_cube, final_score, iterations = algorithm_manager.solve(magic_cube)
+    final_cube, final_score, iterations = algorithm_manager.solve(magic_cube, **kwargs)
     end_time = time.time()
 
     # Display cube state
@@ -71,7 +75,7 @@ while True:
     # Video player animation
     animation_manager = AnimationManager(n, iterations)
     animation_manager.start_animation()
-    
+
     # Ask if the user wants to replay
     while True:
         replay_experiment = input("Apakah Anda ingin melakukan eksperimen ulang (y atau n): ").strip().lower()
