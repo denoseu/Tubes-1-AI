5ca3377579b796e40d4fddba886cc05a18bb2a34
angiekierra <angie.kierra@gmail.com>
2024-11-04 19:20:19 +0700

    fix: genetic algorithm

--- src/genetic.py
+++ src/genetic.py
@@ -5,34 +5,53 @@ from strategy import AlgorithmStrategy
 from cube import MagicCube
 
 class GeneticAlgorithm(AlgorithmStrategy):
-    def execute(self, magic_cube,population_size, generations, mutation_rate,n):
+    def execute(self, magic_cube, population_size, generations,n):
         population = [MagicCube(n) for _ in range(population_size)]
         best_individual = None
         best_fitness= float('-inf')
+        best_obj_function = float('-inf')
         iterations = []
+        scores = []
+        avg_fitness = []
 
-        for generation in range(generations):
+        for i in range(generations):
             sumofObjectiveFunctions = self._getSumOfObjectiveFunctions(population)
             fitness_scores = [(abs(cube.getCurrentScore())/sumofObjectiveFunctions) for cube in population]
-            print(f"fitness scores {fitness_scores}")
-
             
             # Find best individual in current generation
             current_best_fitness = max(fitness_scores)
             current_best_individual = population[np.argmax(fitness_scores)]
+            iterations.append((current_best_individual.cube.copy(), current_best_individual.getCurrentScore(),current_best_fitness))
 
             if current_best_fitness > best_fitness:
                 best_fitness = current_best_fitness
                 best_individual = current_best_individual.cube.copy()
+                best_obj_function = current_best_individual.getCurrentScore()
+
+            scores.append(best_fitness)
+            avg_fitness.append(np.mean(fitness_scores))
 
             new_population = []
-            for _ in range(1):
+            for _ in range(population_size // 2):
                 parent1 = self._roulette_wheel_selection(population, fitness_scores)
                 parent2 = self._roulette_wheel_selection(population, fitness_scores)
-                print(parent1.cube)
-                child1, child2 = self._ordered_crossover(parent1,parent2)
-                print(child1.cube)
+                child1,child2 = self._ordered_crossover(parent1,parent2)
 
+                self._mutate(child1)
+                self._mutate(child2)
+
+                new_population.extend([child1,child2])
+            population = new_population
+        
+         # Final results
+        print("\nFinal Results:")
+        print(f"Best Individual Fitness: {best_fitness}")
+        print(f"Best Obj Function {best_obj_function}")
+        print("Iterations (Best Individual, Fitness):")
+        for i, (ind,obj, fit) in enumerate(iterations):
+            print(f"Iteration {i + 1}: OBJ = {obj} Fitness = {fit}")
+
+        return best_individual,best_fitness,iterations
 
 
     def _getSumOfObjectiveFunctions(self,population):
@@ -51,11 +70,8 @@ class GeneticAlgorithm(AlgorithmStrategy):
         parent1_flatten = parent1.cube.flatten()
         parent2_flatten = parent2.cube.flatten()
 
-
         crossover_start = random.randint(0, len(parent1_flatten) - 2)
         crossover_end = random.randint(crossover_start + 1, len(parent1_flatten) - 1)
-
-        print(crossover_end)
 
         #placeholder tracker mana yg blom diisi
         child1_flatten = [-1] * len(parent1_flatten)
@@ -64,8 +80,10 @@ class GeneticAlgorithm(AlgorithmStrategy):
         child1_flatten[crossover_start:crossover_end] = parent1_flatten[crossover_start:crossover_end]
         child2_flatten[crossover_start:crossover_end] = parent2_flatten[crossover_start:crossover_end]
 
-        self._fill_remaining_values(child1_flatten, parent2_flatten,crossover_end)
-        self._fill_remaining_values(child2_flatten, parent1_flatten,crossover_end)
+
+        self._fill_remaining_values(child1_flatten, parent2_flatten,0)
+        self._fill_remaining_values(child2_flatten, parent1_flatten,0)
+
 
         child1_cube = np.array(child1_flatten).reshape((n, n, n))
         child2_cube = np.array(child2_flatten).reshape((n, n, n))
@@ -77,19 +95,26 @@ class GeneticAlgorithm(AlgorithmStrategy):
 
         child1.initialize_sums()
         child2.initialize_sums()
+        child1.update_position_map()
+        child2.update_position_map()
         
-        return child1, child2
+        return child1,child2
 
     def _fill_remaining_values(self,child,source,pos):
         for gene in source:
             if gene not in child:
-                if pos == len(child) - 1:
-                    pos = 0  # Wrap around to the beginning if we reach the end
-                
-                while child[pos] != -1:  # Find the next empty position
+                while child[pos] != -1: 
                     pos += 1
                 child[pos] = gene
 
+    def _mutate(self, cube):
+        if random.random() < 0.3:
+            num1 = random.randint(1, 125)
+            num2 = random.randint(1, 125)
+            while num2 == num1:
+                num2 = random.randint(1, 125)
+
+            cube.swap_number(num1, num2)
 
 
 
--- src/test.py
+++ src/test.py
@@ -4,7 +4,7 @@ from cube import MagicCube  # Assuming MagicCube is implemented and imported cor
 
 # Define parameters
 population_size = 10        # Set the size of the population
-generations = 1           # Set the number of generations to evolve
+generations = 10000         # Set the number of generations to evolve
 mutation_rate = 0.1         # Set the mutation rate
 n = 5                       # Size of the magic cube (for example, a 5x5x5 cube)
 
@@ -15,4 +15,4 @@ genetic_algorithm = GeneticAlgorithm()
 initial_magic_cube = MagicCube(n)
 
 # Run the genetic algorithm
-genetic_algorithm.execute(initial_magic_cube, population_size, generations, mutation_rate, n)
+genetic_algorithm.execute(initial_magic_cube, population_size, generations, n)
